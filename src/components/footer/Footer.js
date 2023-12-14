38bd2cc04b4efafd9444dcca05b1d88107ace0b4
MetelevNikita <Kyle.B@mail.ru>
2023-12-14 14:46:32 +0500

    release 2.0

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,6 +12,9 @@ const Footer = () => {
           <div className="footer-container">
             <div className="footer-box">
 
+
+                <div className="footer-release">release 1.0</div>
+
                 <div className="footer-title">ТЕХНИЧЕСКАЯ ПОДДЕРЖКА: 8 989 951 90 63</div>
                 <div className="footer-subtitle">Производство Метелев Никита </div>
 
