16935d566305731d56210ae1333304442f399e38
NikitaMetelev <Kyle.B@mail.ru>
2025-08-04 17:35:57 +0500

    new design desk

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
             <div className="footer-box">
 
 
-                <div className="footer-release">version 2.4</div>
+                <div className="footer-release">version 2.6</div>
 
                 <Row className='d-flex justify-content-center mt-3 mb-3'>
 
